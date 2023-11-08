f7ddb7fa14dbc7785aa4944c773c52de5257014d
Youngs5 <oys9330@gmail.com>
2023-11-08 12:35:30 +0900

    Feat : 프로젝트 생성

---
+++ ZoomMonitoring/ZoomMonitoring/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ZoomMonitoring
+//
+//  Created by 오영석 on 11/8/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ ZoomMonitoring/ZoomMonitoring/ViewController/MainViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewController.swift
+//  ZoomMonitoring
+//
+//  Created by 오영석 on 11/8/23.
+//
+
+import Foundation
