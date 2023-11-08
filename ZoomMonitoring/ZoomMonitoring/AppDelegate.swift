6a1437acb5e4acc75bab2f5d5aaf8b8746834c62
Youngs5 <oys9330@gmail.com>
2023-11-08 12:59:16 +0900

    Feat: AppDelegate 수정

--- ZoomMonitoring/ZoomMonitoring/AppDelegate.swift
+++ ZoomMonitoring/ZoomMonitoring/AppDelegate.swift
@@ -1,30 +1,25 @@
 //
 //  AppDelegate.swift
-//  ZoomMonitoring
+//  Monitoring
 //
-//  Created by 오영석 on 11/8/23.
+//  Created by 오영석 on 11/2/23.
 //
 
 import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    
-
+    var window: NSWindow!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let contentViewController = MainViewController()
+        window = NSWindow(contentViewController: contentViewController)
+        window.title = "Monitoring System"
+        window.setContentSize(NSSize(width: 800, height: 600))
+        window.center()
+        window.makeKeyAndOrderFront(nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
-    }
-
-
 }
-
