d94a872d16bd25e9c1246ea4688110dcacc6c2dc
Youngs5 <oys9330@gmail.com>
2023-11-10 20:23:12 +0900

    Refactor : 기능 함수 Store로 분리

--- ZoomMonitoringSwiftUI/ZoomMonitoringSwiftUI/ContentView.swift
+++ ZoomMonitoringSwiftUI/ZoomMonitoringSwiftUI/ContentView.swift
@@ -5,32 +5,24 @@
 //  Created by 오영석 on 11/10/23.
 //
 
-//
-//  ContentView.swift
-//  Facelook
-//
-//  Created by Jongwook Park on 11/9/23.
-//
-
 import SwiftUI
 import AppKit
 import Vision
 
 struct ContentView: View {
-    @State private var image: NSImage?
-    @State private var faceObservations: [VNFaceObservation] = []
+    @ObservedObject private var zoomStore = ZoomStore()
     
     var body: some View {
         VStack {
             Button("Capture Screen") {
-                captureScreen()
+                zoomStore.captureScreen()
             }
-            if let image = image {
+            if let image = zoomStore.image {
                 ZStack {
                     Image(nsImage: image)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                    ForEach(faceObservations, id: \.self) { observation in
+                    ForEach(zoomStore.faceObservations, id: \.self) { observation in
                         Rectangle()
                             .stroke(Color.green, lineWidth: 4)
                             .frame(width: observation.boundingBox.width * image.size.width, height: observation.boundingBox.height * image.size.height)
@@ -39,47 +31,17 @@ struct ContentView: View {
                 }
             }
             
-            
-            if faceObservations.isEmpty {
+            if zoomStore.faceObservations.isEmpty {
                 Text("Hello")
             } else {
-                Text("Number of faces: \(faceObservations.count)")
-//                    .font(.largeTitle)
-//                    .foregroundColor(.white)
-//                    .padding()
-//                    .background(Color.black.opacity(0.7))
-//                    .cornerRadius(10)
-//                    .offset(y: -image.size.height / 2 + 50)
+                Text("Number of faces: \(zoomStore.faceObservations.count)")
             }
-        }
-    }
-    
-    func captureScreen() {
-        let displayID = CGMainDisplayID()
-        guard let imageRef = CGDisplayCreateImage(displayID) else { return }
-        image = NSImage(cgImage: imageRef, size: NSSize(width: imageRef.width, height: imageRef.height))
-        detectFaces()
-    }
-    
-    func detectFaces() {
-        guard let image = image else { return }
-        guard let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil) else { return }
-        let request = VNDetectFaceRectanglesRequest { request, error in
-            if let error = error {
-                print("Face detection error: \(error.localizedDescription)")
-            }
-            guard let results = request.results as? [VNFaceObservation] else { return }
-            self.faceObservations = results
-        }
-        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-        do {
-            try handler.perform([request])
-        } catch {
-            print("Face detection request error: \(error.localizedDescription)")
         }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- ZoomMonitoringSwiftUI/ZoomMonitoringSwiftUI/Store.swift
+++ ZoomMonitoringSwiftUI/ZoomMonitoringSwiftUI/Store.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import AppKit
+import Vision
+
+class ZoomStore: ObservableObject {
+    @Published var image: NSImage?
+    @Published var faceObservations: [VNFaceObservation] = []
+    
+    func captureScreen() {
+        let displayID = CGMainDisplayID()
+        guard let imageRef = CGDisplayCreateImage(displayID) else { return }
+        image = NSImage(cgImage: imageRef, size: NSSize(width: imageRef.width, height: imageRef.height))
+        detectFaces()
+    }
+    
+    func detectFaces() {
+        guard let image = image else { return }
+        guard let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil) else { return }
+        let request = VNDetectFaceRectanglesRequest { request, error in
+            if let error = error {
+                print("Face detection error: \(error.localizedDescription)")
+            }
+            guard let results = request.results as? [VNFaceObservation] else { return }
+            self.faceObservations = results
+        }
+        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Face detection request error: \(error.localizedDescription)")
+        }
+    }
+}
